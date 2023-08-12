3ffafa7cd6044dc97ddfab92e28be079eca18f2b
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2023-08-12 20:16:15 +0300

    Initial Commit

---
+++ MachineLearningImageRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognitionApp
+//
+//  Created by Abdullah Karagöz on 12.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
